7b5824f43a278164a6d95cb0c0c2ed25b7f3613f
ellambg <ellalina1066@gmail.com>
2025-10-09 11:08:04 +0200

    WIP

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-
+import { ref } from 'vue'
+ 
+const counter = ref(0);
+const increment = () => {
+counter.value++
+ 
+}
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
   <h1>Welcome</h1>
+  <p>Counter: {{counter}}</p>
+  <button @click="increment"></button>
 </template>
